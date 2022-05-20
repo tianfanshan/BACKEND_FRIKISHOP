f375065bc0f56c3c7637ada40be89dc9531e1486
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-20 13:02:13 +0200

    clean

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ app.use(express.json());
 
 app.use('/users', require('./routes/users'));
 app.use('/sets', require('./routes/sets'));
-
 app.use('/categories', require('./routes/categories'))
 
 app.listen(port, () => console.log(`Servidor levantado sobre el puerto ${port}`))
