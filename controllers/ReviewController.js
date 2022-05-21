89a25baab4e861bf7ace74268725c3c11785f84b
Vanesa <vaneebg@gmail.com>
2022-05-21 23:38:54 +0200

    hecho crud de review correct

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -1,4 +1,4 @@
-const { Review } = require('../models/index.js')
+const { Review, Product } = require('../models/index.js')
 
 const ReviewController = {
     async create(req, res) {
@@ -8,6 +8,45 @@ const ReviewController = {
         } catch (error) {
             console.log(error);
             res.send('Algo ha salido mal...')
+        }
+    },
+    async findAll(req, res) {
+        try {
+            res.send(
+                await Review.findAll({ include: [Product] })
+            )
+        } catch (error) {
+
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+    },
+    async update(req, res) {
+        try {
+            await Review.update({...req.body }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            res.send(`Review con id ${req.params.id} actualizado con éxito`);
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+
+    },
+    async delete(req, res) {
+        try {
+            await Review.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.send(`La review con id ${req.params.id}  ha sido eliminada con éxito`)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
         }
     }
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,5 +3,8 @@ const ReviewController = require('../controllers/ReviewController');
 const router = express.Router();
 
 router.post('/', ReviewController.create)
+router.get('/', ReviewController.findAll)
+router.put('/id/:id', ReviewController.update)
+router.delete('/id/:id', ReviewController.delete)
 
 module.exports = router;
