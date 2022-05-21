0bd8ed0ea2c3705852d571225b727cd0761720ac
Vanesa <vaneebg@gmail.com>
2022-05-21 20:12:59 +0200

    seeds reviews

---
+++ seeders/20220521180557-demo-review.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert('Reviews', [{
+            "title": "mi review 1",
+            "body": "Cuerpo de la reviewwwwwwwwwwwwwww ",
+            "score": 3,
+            "UserId": 1,
+            "ProductId": 2,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }, {
+            "title": "mi review 2",
+            "body": "Cuerpo de la reviewwwwwwwwwwwwwww ",
+            "score": 7,
+            "UserId": 2,
+            "ProductId": 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }, {
+            "title": "mi review 3",
+            "body": "Cuerpo de la reviewwwwwwwwwwwwwww ",
+            "score": 9,
+            "UserId": 3,
+            "ProductId": 6,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }, {
+            "title": "mi review 4",
+            "body": "Cuerpo de la reviewwwwwwwwwwwwwww ",
+            "score": 9,
+            "UserId": 4,
+            "ProductId": 8,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }, {
+            "title": "mi review 5",
+            "body": "Cuerpo de la reviewwwwwwwwwwwwwww ",
+            "score": 10,
+            "UserId": 2,
+            "ProductId": 7,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }])
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    }
+};
