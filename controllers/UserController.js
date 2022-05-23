6a1068be61f8d2154cdb4f84619c7bd89adb71a0
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-23 12:11:12 +0200

    password Hassed

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,13 @@
-const { User } = require('../models/index.js')
+const { User } = require('../models/index.js');
+const bcrypt = require('bcryptjs');
 
 const UserController = {
     async create(req, res) {
         try {
-            req.body.role = "user" //TODO hay que modificar la tabla
-            await User.create({...req.body })
-            res.status(201).send('Se ha añadido correctamente')
+            req.body.role = "user" //TODO hay que modificar el diagrama
+            const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+            const user = await User.create({...req.body, password: hashedPassword });
+            res.status(201).send({ message: 'Se ha creado un usuario', user })
         } catch (error) {
             console.log(error);
             res.send('Algo ha salido mal...')
