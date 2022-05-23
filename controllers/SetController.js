f6c2f5402f5447af1ea43dca01de331f2516de6e
Vanesa <vaneebg@gmail.com>
2022-05-23 19:54:09 +0200

    hecho controllers y rutas para CRUD sets

--- controllers/SetController.js
+++ controllers/SetController.js
@@ -1,15 +1,115 @@
-const { Set } = require('../models/index.js')
+const { Set, Categorie, Review, Sequelize } = require('../models/index.js')
+const { Op } = Sequelize;
 
 const SetController = {
     async create(req, res) {
         try {
-            await Set.create({...req.body })
-            res.status(201).send('Se ha añadido correctamente')
+            const newSet = await Set.create({...req.body })
+            res.status(201).send({ message: 'Se ha añadido el Set correctamente', newSet })
         } catch (error) {
             console.log(error);
             res.send('Algo ha salido mal...')
         }
+    },
+    // async findAll(req, res) {
+    //     try {
+    //         res.send(
+    //             await Set.findAll({ include: [Categorie] })
+    //         )
+    //     } catch (error) {
+
+    //         console.log(error)
+    //         res.status(500).send({ message: 'Ha habido un problema ' })
+    //     }
+    // },
+    async update(req, res) {
+        try {
+            await Set.update({...req.body }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            const SetUpdated = await Set.findByPk(req.params.id);
+            res.send({ message: `Set con id ${req.params.id} actualizado con éxito`, SetUpdated });
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+
+    },
+    async delete(req, res) {
+        try {
+            await Set.destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+
+            res.send(`El Set con id ${req.params.id} ha sido eliminado con éxito`)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+
+    },
+    // async getById(req, res) {
+    //     try {
+    //         res.send(
+    //             await Set.findByPk(req.params.id, { include: [Categorie] })
+    //         )
+    //     } catch (error) {
+    //         console.log(error)
+    //         res.status(500).send({ message: 'Ha habido un problema ' })
+    //     }
+    // },
+    async getOneByName(req, res) {
+        try {
+            res.send(await Set.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${req.params.name}%`
+                    }
+                },
+                // include: [Categorie]
+            }))
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+
+    },
+    async filterByPrice(req, res) {
+        try {
+            res.send(await Set.findAll({
+                limit: 200,
+                where: {
+                    price: {
+                        [Op.gte]: [`${+req.params.min}`],
+                        [Op.lte]: [`${+req.params.max}`],
+                    }
+                },
+                include: [Categorie]
+            }))
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
+    },
+    async orderPrice(req, res) {
+        try {
+            res.send(
+                await Set.findAll({
+                    order: [
+                        ['price', 'DESC']
+                    ]
+                }, { include: [Categorie] })
+            )
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ message: 'Ha habido un problema ' })
+        }
     }
+
 }
 
 module.exports = SetController
--- routes/sets.js
+++ routes/sets.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const { authentication, isAdmin } = require('../middelware/authentication')
 
 router.post('/', authentication, isAdmin, SetController.create)
-    //* CRUD
-    //* Endpoint que extraiga todos los sets con sus productos
-    //* Endpoint que extraiga todas las categorias que hay en los sets
+router.put('/id/:id', authentication, isAdmin, SetController.update)
+router.delete('/id/:id', authentication, isAdmin, SetController.delete)
+router.get('/name/:name', SetController.getOneByName)
+
+//* Endpoint que extraiga todos los sets con sus productos
+//* Endpoint que extraiga todas las categorias que hay en los sets
 
 
 module.exports = router;
