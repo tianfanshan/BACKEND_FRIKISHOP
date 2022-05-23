d1d9b56dcbed986d2a21a7fd2293cf15cf6c8798
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-23 13:39:50 +0200

    creada carpeta middleware y hecho archivo js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
 const router = express.Router();
-
+const { authentication } = require('../middelware/authentication')
 router.post('/', UserController.create)
 router.get('/login', UserController.login)
+
+
+
 
 module.exports = router;
