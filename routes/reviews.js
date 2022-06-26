ccbd06f699d1f94d98756dab1f469bbb409b36f9
Vanesa Beltrán Guillén <vaneebg@gmail.com>
2022-06-26 13:37:37 +0200

    quitado auth reveiws

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,7 +5,7 @@ const { authentication, isAdmin } = require('../middelware/authentication')
 
 router.post('/', authentication, ReviewController.create)
 router.get('/', authentication, isAdmin, ReviewController.findAll)
-router.get('/review_product', authentication, ReviewController.findAllProduct)
+router.get('/review_product', ReviewController.findAllProduct)
 router.get('/review_product/id/:id', ReviewController.getById)
 router.put('/id/:id', authentication, ReviewController.update)
 router.delete('/id/:id', authentication, ReviewController.delete)
